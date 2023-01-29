484f8afc14508d9f857d43c46c422cd6726dbc23
ge.tang <2662530690@qq.com>
2023-01-29 16:17:44 +0800

    util

---
+++ util.js
@@ -0,0 +1,8 @@
+// 清除收集的函数依赖
+function cleanup(effectFn) {
+  for (let i = 0; i < effectFn.deps.length; i++) {
+    const deps = effectFn.deps[i];
+    deps.delete(effectFn);
+  }
+  effectFn.deps.length = 0;
+}
